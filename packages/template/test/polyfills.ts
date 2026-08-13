bb6510afa82b0a772f985653caf74801d3d19b1c
Manvi Gupta <guptamanvi@google.com>
2026-08-13 21:47:07 +0000

    test(template): fully migrate @firebase/template to single unified Vitest config for Node and Browser

--- packages/template/test/polyfills.ts
+++ packages/template/test/polyfills.ts
@@ -26,3 +26,13 @@ if (typeof (globalThis as Record<string, unknown>).process === 'undefined') {
     env: {}
   };
 }
+
+if (typeof (globalThis as any).before === 'undefined') {
+  (globalThis as any).before = (globalThis as any).beforeAll;
+}
+if (typeof (globalThis as any).after === 'undefined') {
+  (globalThis as any).after = (globalThis as any).afterAll;
+}
+if (typeof (globalThis as any).context === 'undefined') {
+  (globalThis as any).context = (globalThis as any).describe;
+}
